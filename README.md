e08ccea8251f6901a270061fa6b8971dbea46572
Patrick Sicking <Patrick.Sicking@netgo.de>
2021-02-18 14:05:58 +0100

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # signalr-api-example
+A simple example for a signalr api with one channel.
+That channel can be used to exchange text messages.
+
+You can use it with the accompanying signalr-win-client-example repository.
---
+++ SignalRApi/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRApi.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, Customer customer = null)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
